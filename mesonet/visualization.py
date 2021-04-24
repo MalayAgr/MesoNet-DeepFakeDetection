9e2441464b184295cdc496bc3321a1c3862b93b7
Malay Agarwal <malay.agarwal261016@outlook.com>
2021-04-25 01:48:58 +0530

    Fix bugs and fix docstring formatting

--- mesonet/visualization.py
+++ mesonet/visualization.py
@@ -9,7 +9,7 @@ def plot_loss_curve(history):
     Function to plot training and validation loss of a trained model.
 
     Args:
-        history (`History` object): The history of the model.
+        history (`History` object): History of the model.
     """
     plt.plot(history.history["loss"], "r", label="train")
     plt.plot(history.history["val_loss"], "g", label="validation")
@@ -27,8 +27,9 @@ def _visualize_conv_layers_single_img(
     Function to visualize output of multiple conv layers for a single image.
 
     Args:
-        activations (Numpy array): The computed outputs of conv layers for a image
-        conv_idx (List-like): The indices of the conv layers.
+        activations (list-like): Computed outputs of conv layers for a image. It should be list-like containing Numpy arrays.
+        conv_idx (list-like): Indices of the conv layers to be visualized (0-indexed).
+            The plots will be generated in the order the indices are mentioned..
     """
     images_per_row = 4
 
@@ -40,12 +41,12 @@ def _visualize_conv_layers_single_img(
         num_rows = num_filters // images_per_row
 
         fig = plt.figure()
+        fig.suptitle(f"Convolutional Layer {idx + 1}")
         grid = ImageGrid(fig, 111, (num_rows, images_per_row))
 
         for ax, im in zip(grid, imgs):
             ax.imshow(im, cmap="viridis")
 
-        plt.title(f"Convolutional Layer {idx + 1}")
         plt.show()
 
 
@@ -54,19 +55,21 @@ def visualize_conv_layers(model, imgs, conv_idx):
     Function to visualize specified conv layers for given images.
 
     Args:
-        model (tf.keras.Model): The model whose layers are to be visualized.
-        imgs (Numpy array): The images for which the layers are to be visualized.
+        model (tf.keras.Model): Model whose layers are to be visualized.
+        imgs (Numpy array): Images for which the layers are to be visualized.
             The dimension of the array should be (x, HEIGHT, WIDTH, CHANNELS), where
             x is the number of images. HEIGHT, WIDTH and CHANNELS should match the
             inputs for the model.
-        conv_idx (list-like): The indices of the conv layers to be visualized (0-indexed).
+        conv_idx (list-like): Indices of the conv layers to be visualized (0-indexed).
             The plots will be generated in the order the indices are mentioned.
     """
+    num_layers = len(conv_idx)
+
     activation_model = get_activation_model(model, conv_idx)
     activations = activation_model.predict(imgs)
+    activations = [activations] if num_layers == 1 else activations
 
     num_imgs = imgs.shape[0]
-    num_layers = len(conv_idx)
 
     for idx in range(num_imgs):
         img_activs = [activations[i][idx, :, :, :] for i in range(num_layers)]
